b97ef51038689f6da1862e8d78bca0073b8b3f95
enriquop <44776832+enriquop@users.noreply.github.com>
2020-11-29 22:26:21 +0100

    Add files via upload

---
+++ Get Computer name/computername.bat
@@ -0,0 +1,11 @@
+@echo off
+REM THIS FUNCTION IS USED TO GET THE COMPUTER NAME
+Title Computername
+echo %Computername%
+REM You can add @exit at the end to exit the CMD window when enter is pressed
+pause
+
+
+
+
+REM CREATED BY: Thedoc
